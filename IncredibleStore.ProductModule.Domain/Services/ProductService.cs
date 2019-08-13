47e5c07a5028cfbf25dab9c396210d6b7cae14dd
Gustavo de Miranda <gustavomirandag@gmail.com>
2019-08-12 22:22:08 -0300

    Added the product service with use cases.

---
+++ IncredibleStore.ProductModule.Domain/Services/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using IncredibleStore.ProductModule.Domain.Entities;
+using IncredibleStore.ProductModule.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace IncredibleStore.ProductModule.Domain.Services
+{
+    public class ProductService
+    {
+        private IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void RegisterProduct(Product product)
+        {
+            _productRepository.Create(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _productRepository.Update(product);
+        }
+
+        public Product GetProduct(Guid id)
+        {
+            return _productRepository.Read(id);
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _productRepository.ReadAll();
+        }
+
+        public IEnumerable<Product> GetProductsByName(string name)
+        {
+            return GetAllProducts().Where(product => product.Name
+                .ToLower()
+                .Contains(name.ToLower())
+                );
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            _productRepository.Delete(product);
+        }
+    } 
+}
