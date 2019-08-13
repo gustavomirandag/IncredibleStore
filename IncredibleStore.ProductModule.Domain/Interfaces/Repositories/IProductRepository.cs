a3ba11fe08bf453d73a5edbb507af0e9ed725081
Gustavo de Miranda <gustavomirandag@gmail.com>
2019-08-12 22:12:35 -0300

    Started the implementation of the IncredibleStore for education purposes.

---
+++ IncredibleStore.ProductModule.Domain/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using IncredibleStore.ProductModule.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IncredibleStore.ProductModule.Domain.Interfaces.Repositories
+{
+    public interface IProductRepository
+    {
+        void Create(Product product);
+        Product Read(Guid id);
+        IEnumerable<Product> ReadAll();
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
---
+++ IncredibleStore.ProductModule.Infra.DataAccess/Repositories/ProductSqlServerRepository.cs
@@ -0,0 +1,36 @@
+﻿using IncredibleStore.ProductModule.Domain.Entities;
+using IncredibleStore.ProductModule.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IncredibleStore.ProductModule.Infra.DataAccess.Repositories
+{
+    public class ProductSqlServerRepository : IProductRepository
+    {
+        public void Create(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Read(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
